25f8a93993befeb4b7ba2874ff1c7409438d52a6
Giuseppe Cutuli <cutuligiuseppe95@gmail.com>
2025-04-06 15:54:13 +0200

    feat(docker): completed setup for prod environment

--- apps/api/src/config/configuration.ts
+++ apps/api/src/config/configuration.ts
@@ -3,16 +3,16 @@ import { Config } from './config.interface'
 
 export default (): Config => ({
   nest: {
-    port: getOsEnvNumber('port', 3000),
+    port: getOsEnvNumber('PORT', 3000),
   },
   cors: {
     enabled: true,
   },
   jwt: {
     secret: getOsEnv('JWT_SECRET'),
-    expiresIn: getOsEnv('JWT_EXPIRES_IN'),
+    expiresIn: getOsEnv('JWT_EXPIRES_IN', '1h'),
     refreshSecret: getOsEnv('JWT_REFRESH_SECRET'),
-    refreshExpiresIn: getOsEnv('JWT_REFRESH_EXPIRES_IN'),
+    refreshExpiresIn: getOsEnv('JWT_REFRESH_EXPIRES_IN', '30d'),
   },
   swagger: {
     enabled: true,
